ae792afdd16b2061cd0a9636a88c243d69d9e509
Drew MacInnis <drew@dreamdrew.ca>
2017-09-03 18:59:00 -0400

    Fix tests

--- main_test.go
+++ main_test.go
@@ -18,8 +18,8 @@ func TestInitViper(t *testing.T) {
 	v := initViper(viper.New(), "syncrets-test1", "testdata/")
 	assert.NotNil(t, v.Sub("vault"))
 	assert.Equal(t, v.Get("vault.vault-a.url"), "http://localhost:8201")
-	assert.Equal(t, v.Get("vault.vault-a.auth-method"), "token")
-	assert.Equal(t, v.Get("vault.vault-a.token-file"), ".vault-a-token")
+	assert.Equal(t, v.Get("vault.vault-a.auth.method"), "token")
+	assert.Equal(t, v.Get("vault.vault-a.token.file"), ".vault-a-token")
 	assert.Equal(t, v.Get("vault.vault-b.url"), "http://localhost:8202")
 }
 
