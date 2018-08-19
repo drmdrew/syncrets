bf7f2acaa3f225b4dcaec58e741a2da043446732
Drew MacInnis <drew@dreamdrew.ca>
2018-08-19 19:41:02 -0400

    Add integration tests with docker-compose

--- integration_test.go
+++ integration_test.go
@@ -11,25 +11,56 @@ import (
 	"testing"
 )
 
-func TestIntegration_SyncretsList(t *testing.T) {
-	expected := []string{
-		"/secret/foo", "/secret/gilbert", "/secret/foo/bar", "/secret/it/was/the/best/of/times"}
-	cmdName := "./syncrets"
-	cmdArgs := []string{"list", "vault://vault-a/secret/"}
+func execCommand(t *testing.T, cmdName string, cmdArgs []string) []string {
 	var cmdOut []byte
 	var err error
 	if cmdOut, err = exec.Command(cmdName, cmdArgs...).Output(); err != nil {
-		fmt.Fprintf(os.Stderr, "There was an error running syncrets: %v\n%v", err, string(cmdOut))
+		fmt.Fprintf(os.Stderr, "%v failed: %v\n%v", cmdName, err, string(cmdOut))
 		t.FailNow()
 	}
 	scanner := bufio.NewScanner(bytes.NewReader(cmdOut))
-	var actual []string
+	var output []string
 	for scanner.Scan() {
-		actual = append(actual, scanner.Text())
+		output = append(output, scanner.Text())
 	}
+	return output
+}
+
+/* libcompose doesn't work with docker-compose version 3 */
+/*
+func dockerComposeSetup() {
+	project, err := docker.NewProject(&ctx.Context{
+		Context: project.Context{
+			ComposeFiles: []string{"docker-compose.yml"},
+			ProjectName:  "syncrets",
+		},
+	}, nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = project.Up(context.Background(), options.Up{})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+*/
+
+func dockerComposeSetup(t *testing.T) {
+	execCommand(t, "docker-compose", []string{"up", "-d"})
+}
+
+func TestIntegration_SyncretsList(t *testing.T) {
+	dockerComposeSetup(t)
+	expected := []string{
+		"/secret/foo", "/secret/gilbert", "/secret/foo/bar", "/secret/it/was/the/best/of/times",
+	}
+	actual := execCommand(t, "./syncrets", []string{"list", "vault://vault-a/secret/"})
 	for i, key := range actual {
 		if key != expected[i] {
-			fmt.Fprintf(os.Stderr, "line %d: expected '%v', got '%v'", expected[i], key)
+			fmt.Fprintf(os.Stderr, "%d: expected '%v', got '%v'", i, expected[i], key)
 			t.FailNow()
 		}
 	}
