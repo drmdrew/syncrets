95d69f7a1f16eccd7a02a2f050d4d8e0d8a0ca3d
Drew MacInnis <drew@dreamdrew.ca>
2018-08-19 20:15:58 -0400

    Correct vault-a-token expection

--- main_test.go
+++ main_test.go
@@ -19,7 +19,7 @@ func TestInitViper(t *testing.T) {
 	assert.NotNil(t, v.Sub("vault"))
 	assert.Equal(t, v.Get("vault.vault-a.url"), "http://localhost:8201")
 	assert.Equal(t, v.Get("vault.vault-a.auth.method"), "token")
-	assert.Equal(t, v.Get("vault.vault-a.token.file"), ".vault-a-token")
+	assert.Equal(t, v.Get("vault.vault-a.token.file"), "vault-a-token")
 	assert.Equal(t, v.Get("vault.vault-b.url"), "http://localhost:8202")
 }
 
